8bc7b905cd11d28aa9ce8a1aaaf87b57ad80e156
alermogit <alermoabout@gmail.com>
2019-12-21 11:50:09 +0300

    tt
    ty

--- src/metrics_server/api/hardware.clj
+++ src/metrics_server/api/hardware.clj
@@ -18,3 +18,23 @@
   "Get hardware metrics"
   []
   (:data (get-metrics-with-http-info)))
+
+;; Задание 1-1
+(defn task11 [metrics]
+      (filter (fn [a] (> (get a :cpuTemp) 2)) metrics))
+
+;; Задание 1-2
+(defn task12 [metrics]
+      (/ (reduce + (map (fn [a] (get a :cpuTemp)) metrics)) (count metrics))
+      )
+;; Задание 1-3
+(defn task13 [metrics]
+      (/ (reduce + (map (fn [a] (get a :cpuLoad)) metrics)) (count metrics))
+      )
+;; Главная функция программы
+(defn -main [& args]
+      (println (get-metrics))
+      (println (task11 (get-metrics)))
+      (println (task12 (get-metrics)))
+      (println (task13 (get-metrics)))
+)
